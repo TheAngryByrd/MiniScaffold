3f0447242a31aed04117316d437e9936467b282f
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-04-14 11:31:23 -0400

    Comment on FrameworkPathOverride

--- Content/build.sh
+++ Content/build.sh
@@ -19,6 +19,7 @@ then
   # (Can't use arrays since dash can't handle them)
   FSIARGS="--fsiargs"
   FSIARGS2="-d:MONO"
+  # Allows NETFramework like net45 to be built using dotnet core tooling with mono
   export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
 
 fi
