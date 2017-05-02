42227f1085c41f2fa8ba32b1e64cd36bbb34c3f0
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-05-01 21:30:07 -0400

    adding inital appveyor script

--- build.cmd
+++ build.cmd
@@ -15,4 +15,9 @@ IF NOT EXIST build.fsx (
   .paket\paket.exe update
   packages\build\FAKE\tools\FAKE.exe init.fsx
 )
+pushd Content\
+call build.cmd
+call build.cmd Clean
+popd
+
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
