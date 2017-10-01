acdb26fc76ad90eaec19bde2ddb79544af7a0966
Andreas Vilinski <vilinski@yahoo.de>
2017-10-01 21:34:13 +0200

    restore template build commands

--- build.cmd
+++ build.cmd
@@ -10,4 +10,10 @@ IF NOT EXIST build.fsx (
   .paket\paket.exe update
   packages\build\FAKE\tools\FAKE.exe init.fsx
 )
+
+pushd Content\
+call build.cmd
+call build.cmd Clean
+popd
+
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
--- build.sh
+++ build.sh
@@ -75,5 +75,12 @@ fi
 
 [ ! -e build.fsx ] && run $PAKET_EXE update
 [ ! -e build.fsx ] && run $FAKE_EXE init.fsx
+
+# build template
+pushd Content/
+./build.sh
+./build.sh Clean
+popd
+
 run $FAKE_EXE "$@" $FSIARGS $FSIARGS2 build.fsx
 
