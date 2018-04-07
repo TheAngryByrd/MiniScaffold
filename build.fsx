86dff3eb941beaf4124a4c8b4b4c3823268a42c1
Chet Husk <chusk3@gmail.com>
2018-04-07 14:56:52 -0500

    initial work to convert to FAKE 5

--- build.fsx
+++ build.fsx
@@ -1,13 +1,13 @@
 #r @"packages/build/FAKE/tools/FakeLib.dll"
-open Fake
-open Fake.Git
-open Fake.AssemblyInfoFile
-open Fake.ReleaseNotesHelper
-open Fake.UserInputHelper
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.IO.FileSystemOperators
 open System
-open System.IO
+open Fake.Tools
 
-let release = LoadReleaseNotes "RELEASE_NOTES.md"
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
 let srcGlob = "*.csproj"
 
 let distDir = __SOURCE_DIRECTORY__  @@ "dist"
@@ -16,38 +16,29 @@ let distGlob = distDir @@ "*.nupkg"
 let gitOwner = "TheAngryByrd"
 let gitRepoName = "MiniScaffold"
 
-Target "Clean" (fun _ ->
+Target.create "Clean" (fun _ ->
     [ "obj" ;"dist"]
-    |> CleanDirs
+    |> Shell.CleanDirs
     )
 
-Target "DotnetRestore" (fun _ ->
+Target.create "DotnetRestore" (fun _ ->
     !! srcGlob
     |> Seq.iter (fun proj ->
-        DotNetCli.Restore (fun c ->
+        DotNet.restore (fun c ->
             { c with
-                Project = proj
-                //This makes sure that Proj2 references the correct version of Proj1
-                AdditionalArgs = [sprintf "/p:PackageVersion=%s" release.NugetVersion]
-            })
+                Common = c.Common |> DotNet.Options.withCustomParams (Some(sprintf "/p:PackageVersion=%s" release.NugetVersion)) }) proj
 ))
 
-
-
-Target "DotnetPack" (fun _ ->
+Target.create "DotnetPack" (fun _ ->
+    let customArgs = DotNet.Options.withCustomParams (Some (sprintf "/p:PackageVersion=%s /p:PackageReleaseNotes=\"%s\"" release.NugetVersion (release.Notes |> String.concat "\n")))
     !! srcGlob
     |> Seq.iter (fun proj ->
-        DotNetCli.Pack (fun c ->
+        DotNet.pack (fun c ->
             { c with
-                Project = proj
-                Configuration = "Release"
-                OutputPath = IO.Directory.GetCurrentDirectory() @@ "dist"
-                AdditionalArgs =
-                    [
-                        sprintf "/p:PackageVersion=%s" release.NugetVersion
-                        sprintf "/p:PackageReleaseNotes=\"%s\"" (String.Join("\n",release.Notes))
-                    ]
-            })
+                Configuration = DotNet.BuildConfiguration.Release
+                OutputPath = IO.Directory.GetCurrentDirectory() @@ "dist" |> Some
+                Common = c.Common |> customArgs
+            }) proj
     )
 )
 
@@ -55,7 +46,7 @@ let dispose (disposable : #IDisposable) = disposable.Dispose()
 [<AllowNullLiteral>]
 type DisposableDirectory (directory : string) =
     do
-        tracefn "Created disposable directory %s" directory
+        Trace.tracefn "Created disposable directory %s" directory
     static member Create() =
         let tempPath = IO.Path.Combine(IO.Path.GetTempPath(), Guid.NewGuid().ToString("n"))
         IO.Directory.CreateDirectory tempPath |> ignore
@@ -66,25 +57,22 @@ type DisposableDirectory (directory : string) =
 
     interface IDisposable with
         member x.Dispose() =
-            tracefn "Deleting directory %s" directory
+            Trace.tracefn "Deleting directory %s" directory
             IO.Directory.Delete(x.Directory,true)
 
 type DisposeablePushd (directory : string) =
-    do FileUtils.pushd directory
+    do Shell.pushd directory
     member x.Directory = directory
     member x.DirectoryInfo = IO.DirectoryInfo(directory)
     interface IDisposable with
         member x.Dispose() =
-            FileUtils.popd()
+            Shell.popd()
 
-
-Target "IntegrationTests" (fun _ ->
+Target.create "IntegrationTests" (fun _ ->
     // uninstall current MiniScaffold
-    DotNetCli.RunCommand id
-        "new -u MiniScaffold"
+    DotNet.exec id "new" "-u MiniScaffold" |> ignore
     // install from dist/
-    DotNetCli.RunCommand id
-        <| sprintf "new -i dist/MiniScaffold.%s.nupkg" release.NugetVersion
+    DotNet.exec id "new" (sprintf "-i dist/MiniScaffold.%s.nupkg" release.NugetVersion) |> ignore
 
     [
         "-n MyCoolLib --githubUsername CoolPersonNo2", "DotnetPack"
@@ -94,36 +82,37 @@ Target "IntegrationTests" (fun _ ->
     |> Seq.iter(fun (param, testTarget) ->
         use directory = DisposableDirectory.Create()
         use pushd1 = new DisposeablePushd(directory.Directory)
-        DotNetCli.RunCommand (fun commandParams ->
-            { commandParams with WorkingDir = directory.Directory}
-        )
-            <| sprintf "new mini-scaffold -lang F# %s" param
+
+        DotNet.exec (DotNet.Options.withWorkingDirectory directory.Directory) "new" (sprintf "mini-scaffold -lang F# %s" param)
+        |> ignore
+
         use pushd2 =
             directory.DirectoryInfo.GetDirectories ()
             |> Seq.head
             |> string
             |> fun x -> new DisposeablePushd(x)
 
+        Git.Repository.init pushd2.Directory false false
+        Git.Staging.stageAll pushd2.Directory
+        Git.CommandHelper.directRunGitCommandAndFail pushd2.Directory (sprintf "remote add origin https://github.com/CoolPersonNo2/%s" gitRepoName)
+        Git.Commit.exec pushd2.Directory "demo commit to make sourcelink work"
         let ok =
-            ProcessHelper.execProcess (fun psi ->
-                psi.WorkingDirectory <- Environment.CurrentDirectory
-                if isMono then
-                    psi.FileName <- "sh"
-                    psi.Arguments <- sprintf "./build.sh %s -nc" testTarget
-                else
-                    psi.FileName <- Environment.CurrentDirectory @@ "build.cmd"
-                    psi.Arguments <- sprintf "%s -nc" testTarget
 
+            Process.execWithResult (fun psi ->
+                { psi with
+                    WorkingDirectory = Environment.CurrentDirectory
+                    FileName = if Environment.isMono then "./build.sh" else Environment.CurrentDirectory @@ "build.cmd"
+                    Arguments = sprintf "%s -nc" testTarget }
                 ) (TimeSpan.FromMinutes(5.))
 
-        if not ok then
-            failwithf "Intregration test failed with params %s" param
+        if not ok.OK then
+            failwithf "Intregration test failed with params %s. Logs:\n%s" param (ok.Results |> Seq.map (fun r -> r.Message) |> String.concat "\n")
     )
 
 )
 
-Target "Publish" (fun _ ->
-    Paket.Push(fun c ->
+Target.create "Publish" (fun _ ->
+    Paket.push(fun c ->
             { c with
                 PublishUrl = "https://www.nuget.org"
                 WorkingDir = "dist"
@@ -131,35 +120,35 @@ Target "Publish" (fun _ ->
         )
 )
 
-Target "GitRelease" (fun _ ->
+Target.create "GitRelease" (fun _ ->
 
     if Git.Information.getBranchName "" <> "master" then failwith "Not on master"
 
     let releaseNotesGitCommitFormat = ("",release.Notes |> Seq.map(sprintf "* %s\n")) |> String.Join
 
-    StageAll ""
-    Git.Commit.Commit "" (sprintf "Bump version to %s \n%s" release.NugetVersion releaseNotesGitCommitFormat)
-    Branches.push ""
+    Git.Staging.stageAll ""
+    Git.Commit.exec "" (sprintf "Bump version to %s \n%s" release.NugetVersion releaseNotesGitCommitFormat)
+    Git.Branches.push ""
 
-    Branches.tag "" release.NugetVersion
-    Branches.pushTag "" "origin" release.NugetVersion
+    Git.Branches.tag "" release.NugetVersion
+    Git.Branches.pushTag "" "origin" release.NugetVersion
 )
 
 #load "paket-files/build/fsharp/FAKE/modules/Octokit/Octokit.fsx"
 open Octokit
 
-Target "GitHubRelease" (fun _ ->
+Target.create "GitHubRelease" (fun _ ->
     let client =
         match Environment.GetEnvironmentVariable "GITHUB_TOKEN" with
         | null ->
             let user =
-                match getBuildParam "github-user" with
+                match Environment.environVarOrDefault "github-user" String.Empty with
                 | s when not (String.IsNullOrWhiteSpace s) -> s
-                | _ -> getUserInput "Username: "
+                | _ -> Fake.UserInputHelper.getUserInput "Username: "
             let pw =
-                match getBuildParam "github-pw" with
+                match Environment.environVarOrDefault "github-pw" String.Empty with
                 | s when not (String.IsNullOrWhiteSpace s) -> s
-                | _ -> getUserPassword "Password: "
+                | _ -> Fake.UserInputHelper.getUserPassword "Password: "
 
             createClient user pw
         | token -> createClientWithToken token
@@ -175,8 +164,9 @@ Target "GitHubRelease" (fun _ ->
 
 )
 
-Target "Release" DoNothing
+Target.create "Release" Target.DoNothing
 
+open Fake.Core.TargetOperators
 "Clean"
   ==> "DotnetRestore"
   ==> "DotnetPack"
@@ -186,4 +176,4 @@ Target "Release" DoNothing
   ==> "GithubRelease"
   ==> "Release"
 
-RunTargetOrDefault "IntegrationTests"
+Target.runOrDefault "IntegrationTests"
