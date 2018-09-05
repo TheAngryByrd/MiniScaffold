3c5b47f3f16e5f9d285b91414879f2370a22f4bd
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2018-09-05 17:29:19 -0400

    Made console app a console app

--- Content/Console/src/MyLib.1/Library.fs
+++
@@ -1,9 +0,0 @@
-namespace MyLib._1
-
-module Say =
-    let nothing name =
-        name |> ignore
-
-    let hello name =
-        sprintf "Hello %s" name
-
---
+++ Content/Console/src/MyLib.1/Program.fs
@@ -0,0 +1,10 @@
+namespace MyLib._1
+open System.Reflection
+
+
+
+
+module Main =
+    [<EntryPoint>]
+    let main (argv : string array) =
+        0
