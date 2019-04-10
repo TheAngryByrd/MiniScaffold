a93268617f74cae09b764cf93a0ab86f8c9dc9cd
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-04-10 16:07:52 -0400

    tests

---
+++ tests/MiniScaffold.Tests/Infrastructure.fs
@@ -0,0 +1,55 @@
+namespace Infrastructure
+
+
+module Dotnet =
+    open Fake.Core
+    open Fake.DotNet
+    let failOnBadExitAndPrint (p : ProcessResult) =
+        if p.ExitCode <> 0 then
+            p.Errors |> Seq.iter Trace.traceError
+            failwithf "failed with exitcode %d" p.ExitCode
+
+    module New =
+        let cmd opt args =
+            let args = args |> String.concat " "
+            DotNet.exec opt "new" args
+            |> failOnBadExitAndPrint
+        let install name =
+            let args = [
+                sprintf "-i %s" name
+            ]
+            cmd id args
+        let uninstall name =
+            let args = [
+                sprintf "-u %s" name
+            ]
+            cmd id args
+
+
+module Disposables =
+    open System
+
+    let dispose (disposable : #IDisposable) = disposable.Dispose()
+
+    [<AllowNullLiteral>]
+    type DisposableDirectory (directory : string) =
+
+        static member Create() =
+            let tempPath = IO.Path.Combine(IO.Path.GetTempPath(), Guid.NewGuid().ToString("n"))
+            IO.Directory.CreateDirectory tempPath |> ignore
+
+            new DisposableDirectory(tempPath)
+        member x.Directory = directory
+        member x.DirectoryInfo = IO.DirectoryInfo(directory)
+
+        interface IDisposable with
+            member x.Dispose() =
+                IO.Directory.Delete(x.Directory, true)
+
+    // type DisposeablePushd (directory : string) =
+    //     do Shell.pushd directory
+    //     member x.Directory = directory
+    //     member x.DirectoryInfo = IO.DirectoryInfo(directory)
+    //     interface IDisposable with
+    //         member x.Dispose() =
+    //             Shell.popd()
--- tests/MiniScaffold.Tests/Main.fs
+++ tests/MiniScaffold.Tests/Main.fs
@@ -1,8 +1,30 @@
 namespace MiniScaffold.Tests
 
+
+
+
 module Main =
+    open Infrastructure
+    open System
+    open Fake.Core
     open Expecto
+    let nugetPkgName =  "MiniScaffold"
+    let nugetPkgPath =
+        match Environment.environVarOrNone "MINISCAFFOLD_NUPKG_LOCATION" with
+        | Some v -> v
+        | None ->
+            let dist = IO.Path.Combine(__SOURCE_DIRECTORY__, "../../dist") |> IO.DirectoryInfo
+            dist.EnumerateFiles("*.nupkg")
+            |> Seq.head
+            |> fun fi -> fi.FullName
+
+
+    let setup () =
+        // ensure we're installing the one from our dist folder
+        Dotnet.New.uninstall nugetPkgName
+        Dotnet.New.install nugetPkgPath
 
     [<EntryPoint>]
     let main argv =
+        setup ()
         Tests.runTestsInAssembly defaultConfig argv
