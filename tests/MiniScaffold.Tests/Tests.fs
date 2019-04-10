7aca433acb67da068d014c531a0765373b34fbb2
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-04-10 13:50:35 -0400

    Scaffold expecto project

---
+++ tests/MiniScaffold.Tests/Tests.fs
@@ -0,0 +1,14 @@
+namespace MiniScaffold.Tests
+
+
+module Tests =
+
+
+    open Expecto
+
+    [<Tests>]
+    let tests =
+      testList "samples" [
+        testCase "Say nothing" <| fun _ ->
+            ()
+      ]
