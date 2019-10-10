d6bdaed295b9026a920a4267e7c47e78607b9eed
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-10-10 18:20:46 -0400

    Proof of concept generating api docs with external libraries

--- Content/Library/build.fsx
+++ Content/Library/build.fsx
@@ -416,6 +416,7 @@ let watchBuild () =
     |> Seq.iter (invokeAsync >> Async.Catch >> Async.Ignore >> Async.Start)
 
 Target.create "ServeDocs" <| fun _ ->
+    watchBuild ()
     DocsTool.watch ()
 
 //-----------------------------------------------------------------------------
