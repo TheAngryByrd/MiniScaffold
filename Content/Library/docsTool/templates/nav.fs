e15d762154d0889786ebdc089318a561bf872005
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-11-01 18:08:53 -0400

    Generate NavTree from filepaths

--- Content/Library/docsTool/templates/nav.fs
+++ Content/Library/docsTool/templates/nav.fs
@@ -1,5 +1,6 @@
 module Nav
 
+open System
 
 open Fable.React
 open Fable.React.Props
@@ -13,9 +14,7 @@ type TopLevelNav = {
     Explanations : list<NameOfArticle * UrlPath>
 }
 
-let generateNav (gitRepoName : string) (topLevelNav : TopLevelNav) =
-
-    let navItem text link =
+let navItem text link =
         li [
             Class "nav-item"
         ] [
@@ -26,184 +25,125 @@ let generateNav (gitRepoName : string) (topLevelNav : TopLevelNav) =
                 span [] [str text]
             ]
         ]
-    let dropDownNavItem text link =
-        li [
-            Class "nav-item"
+
+let dropDownNavMenu text items =
+            li [ Class "nav-item dropdown" ][
+                a [
+                    Id (sprintf "navbarDropdown-%s"  text)
+                    Href "#"
+                    DataToggle "dropdown"
+                    AriaHasPopup true
+                    AriaExpanded false
+                    Class "nav-link dropdown-toggle" ]
+                    [ str text ]
+                ul [    HTMLAttr.Custom ("aria-labelledby", "dropdownMenu1")
+                        Class "dropdown-menu border-0 shadow" ] items ]
+
+let dropDownNavItem text link =
+    li [
+        Class "nav-item"
+    ] [
+        a [
+            Class "dropdown-item"
+            Href link
         ] [
-            a [
-                Class "dropdown-item"
-                Href link
-            ] [
-                span [] [str text]
+            span [] [str text]
+        ]
+    ]
+let dropdownSubMenu text items =
+    li [ Class "dropdown-submenu" ] [
+        a [ Id (sprintf "navbarDropdown-%s"  text)
+            Href "#"
+            Role "button"
+            DataToggle "dropdown"
+            AriaHasPopup true
+            AriaExpanded false
+            Class "dropdown-item dropdown-toggle" ] [
+                str text ]
+        ul [
+            HTMLAttr.Custom ("aria-labelledby", "dropdownMenu2")
+            Class "dropdown-menu border-0 shadow" ] items
+    ]
+
+type NavTree =
+| File of title:string * link:string
+| Folder of title: string * NavTree list
+
+let navTreeFromPaths (rootPath : IO.DirectoryInfo) (files : IO.FileInfo list) =
+    let rec addPath subFilePath parts nodes =
+        // printfn "parts -> %A ... nodes -> %A" parts nodes
+        match parts with
+        | [] -> nodes
+        | hp :: tp ->
+            addHeadPath subFilePath hp tp nodes
+    and addHeadPath subFilePath (hp : string) tp (nodes : NavTree list)=
+        // printfn "hp -> %A ... tp -> %A ... nodes -> %A" hp tp nodes
+        match nodes with
+        | [] -> [
+            if IO.Path.HasExtension hp then
+                File(IO.Path.GetFileNameWithoutExtension hp, subFilePath)
+            else
+                Folder(hp, addPath subFilePath tp [])
             ]
-        ]
-    let dropdownSubMenu text items =
-        li [ Class "dropdown-submenu" ] [
-            a [ Id (sprintf "navbarDropdown-%s"  text)
-                Href "#"
-                Role "button"
-                DataToggle "dropdown"
-                AriaHasPopup true
-                AriaExpanded false
-                Class "dropdown-item dropdown-toggle" ] [
-                    str text ]
-            ul [
-                HTMLAttr.Custom ("aria-labelledby", "dropdownMenu2")
-                Class "dropdown-menu border-0 shadow" ] items
-        ]
+        | Folder(title, subnodes) :: nodes when title = hp -> Folder(title, addPath subFilePath tp subnodes ) :: nodes
+        | hn :: tn -> hn :: addHeadPath subFilePath hp tp tn
+
+    ([], files)
+    ||> List.fold(fun state file ->
+        let subFilePath = file.FullName.Replace(rootPath.FullName, "")
+        let pathParts = subFilePath.Split(IO.Path.DirectorySeparatorChar) |> Array.toList
+        addPath subFilePath pathParts state
+    )
 
 
+let rootpath = IO.DirectoryInfo("/rootthing/another/docs/")
 
-    // let navDropDownItem text href =
-    //     a [
-    //         Class "dropdown-item"
-    //         Href href
-    //     ] [
-    //         str text
-    //     ]
-    // let navDropDown text items =
-    //     li [
-    //         Class "nav-item dropdown"
-    //     ] [
-    //         a [
-    //             Class "nav-link dropdown-toggle"
-    //             Id (sprintf "navbarDropdown-%s"  text)
-    //             Role "button"
-    //             DataToggle "dropdown"
-    //             HTMLAttr.Custom ("aria-haspopup", "true")
-    //             HTMLAttr.Custom ("aria-expanded", "false")
-    //         ] [str text]
-    //         div [
-    //             Class "dropdown-menu"
-    //             HTMLAttr.Custom ("aria-labelledby", (sprintf "navbarDropdown-%s"  text))
-    //         ] items
+let files = [
+    IO.FileInfo(rootpath.FullName + "Tutorials/Getting_Started.html")
+    IO.FileInfo(rootpath.FullName + "Tutorials/Another_Tutorial.html")
+    IO.FileInfo(rootpath.FullName + "Tutorials/OtherThings/Another_Tutorial3.html")
+]
 
-    //     ]
-    // let subNavDropDown text items =
-    //     li [
-    //         Class "dropdown-submenu"
-    //     ] [
-    //         a [
-    //             Class "dropdown-item dropdown-toggle"
-    //             Id (sprintf "navbarDropdown-%s"  text)
-    //             Role "button"
-    //             DataToggle "dropdown"
-    //             HTMLAttr.Custom ("aria-haspopup", "true")
-    //             HTMLAttr.Custom ("aria-expanded", "false")
-    //         ] [str text]
-    //         div [
-    //             Class "dropdown-menu"
-    //             HTMLAttr.Custom ("aria-labelledby", (sprintf "navbarDropdown-%s"  text))
-    //         ] [
-    //             ul [ Class "dropdown-menu border-0 shadow" ] items
-    //         ]
-    //     ]
+let foo = navTreeFromPaths rootpath files
 
-    // nav [ Class "navbar navbar-expand-lg sticky-top navbar-dark bg-dark" ] [
-    //     div [ Class "container" ][
-    //             a [ Href "#"
-    //                 Class "navbar-brand font-weight-bold" ]
-    //                 [ str (gitRepoName) ]
-    //             button [
-    //                 Type "button"
-    //                 DataToggle "collapse"
-    //                 HTMLAttr.Data ("target", "#navbarContent")
-    //                 HTMLAttr.Custom ("aria-controls", "navbars")
-    //                 AriaExpanded false
-    //                 HTMLAttr.Custom ("aria-label", "Toggle navigation")
-    //                 Class "navbar-toggler"
-    //             ] [
-    //                 span [Class "navbar-toggler-icon"] []
-    //             ]
-    //             div [
-    //                 Class "collapse navbar-collapse"
-    //                 Id "navbarContent"
-    //             ] [
-    //                 ul [ Class "navbar-nav mr-auto" ] [
-    //                     li [ Class "nav-item dropdown" ] [
-    //                         a [ Id "dropdownMenu1"
-    //                             Href "#"
-    //                             DataToggle "dropdown"
-    //                             AriaHasPopup true
-    //                             AriaExpanded false
-    //                             Class "nav-link dropdown-toggle" ]
-    //                                 [ str "Dropdown" ]
-    //                     ]
-    //                 ]
-                    //       ul [ HTMLAttr.Custom ("aria-labelledby", "dropdownMenu1")
-                    //            Class "dropdown-menu border-0 shadow" ]
-                    //         [ li [ ]
-                    //             [ a [ Href "#"
-                    //                   Class "dropdown-item" ]
-                    //                 [ str "Some action" ] ]
-                    //           li [ ]
-                    //             [ a [ Href "#"
-                    //                   Class "dropdown-item" ]
-                    //                 [ str "Some other action" ] ]
-                    //           li [ Class "dropdown-divider" ]
-                    //             [ ]
-                    //           li [ Class "dropdown-submenu" ]
-                    //             [ a [ Id "dropdownMenu2"
-                    //                   Href "#"
-                    //                   Role "button"
-                    //                   DataToggle "dropdown"
-                    //                   AriaHasPopup true
-                    //                   AriaExpanded false
-                    //                   Class "dropdown-item dropdown-toggle" ]
-                    //                 [ str "Hover for action" ]
-                    //               ul [ HTMLAttr.Custom ("aria-labelledby", "dropdownMenu2")
-                    //                    Class "dropdown-menu border-0 shadow" ]
-                    //                 [ li [ ]
-                    //                     [ a [ TabIndex -1
-                    //                           Href "#"
-                    //                           Class "dropdown-item" ]
-                    //                         [ str "level 2" ] ]
-                    //                   li [ Class "dropdown-submenu" ]
-                    //                     [ a [ Id "dropdownMenu3"
-                    //                           Href "#"
-                    //                           Role "button"
-                    //                           DataToggle "dropdown"
-                    //                           AriaHasPopup true
-                    //                           AriaExpanded false
-                    //                           Class "dropdown-item dropdown-toggle" ]
-                    //                         [ str "level 2" ]
-                    //                       ul [ HTMLAttr.Custom ("aria-labelledby", "dropdownMenu3")
-                    //                            Class "dropdown-menu border-0 shadow" ]
-                    //                         [ li [ ]
-                    //                             [ a [ Href "#"
-                    //                                   Class "dropdown-item" ]
-                    //                                 [ str "3rd level" ] ]
-                    //                           li [ ]
-                    //                             [ a [ Href "#"
-                    //                                   Class "dropdown-item" ]
-                    //                                 [ str "3rd level" ] ] ] ]
-                    //                   li [ ]
-                    //                     [ a [ Href "#"
-                    //                           Class "dropdown-item" ]
-                    //                         [ str "level 2" ] ]
-                    //                   li [ ]
-                    //                     [ a [ Href "#"
-                    //                           Class "dropdown-item" ]
-                    //                         [ str "level 2" ] ] ] ] ] ]
-                    //   li [ Class "nav-item" ]
-                    //     [ a [ Href "#"
-                    //           Class "nav-link" ]
-                    //         [ str "About" ] ]
-                    //   li [ Class "nav-item" ]
-                    //     [ a [ Href "#"
-                    //           Class "nav-link" ]
-                    //         [ str "Services" ] ]
-                    //   li [ Class "nav-item" ]
-                    //     [ a [ Href "#"
-                    //           Class "nav-link" ]
-                    //         [ str "Contact" ] ]
-                    // ]
+let generateNavMenus (navTree : NavTree list) =
+    let rec innerDo depth (navTree : NavTree list) =
+        navTree
+        |> List.map(fun nav ->
+            match nav with
+            | File (title, link) when depth = 0 -> navItem title link
+            | File (title, link) -> dropDownNavItem title link
+            | Folder (title, subtree) when depth = 0 ->
+                innerDo (depth + 1) subtree
+                |> dropDownNavMenu title
+            | Folder (title, subtree) ->
+                innerDo (depth + 1) subtree
+                |> dropdownSubMenu title
+        )
+    innerDo 0 navTree
 
 
-    //         ]
-    //     ]
-    // ]
+let fakeMenu = [
+    Folder("Hover for action", [
+        File("Level 1 - Item 1" , "#")
+        Folder("Level 1 - Item 2", [
+            File("Level 2 - Item 1" , "#")
+            File("Level 2 - Item 2" , "#")
+            Folder("Level 2 - Item 3", [
+                File("Level 3 - Item 1" , "#")
+                File("Level 3 - Item 2" , "#")
+            ])
+        ])
+        File("Level 1 - Item 3" , "#")
+        File("Level 1 - Item 3" , "#")
 
+    ])
+    File("About" , "#")
+    File("Contact" , "#")
+    File("Services" , "#")
+]
+
+let generateNav (gitRepoName : string) (topLevelNav : TopLevelNav) =
 
     nav [
         Class "navbar navbar-expand-lg sticky-top navbar-dark bg-dark"
@@ -226,38 +166,9 @@ let generateNav (gitRepoName : string) (topLevelNav : TopLevelNav) =
         div [   Class "collapse navbar-collapse"
                 Id "navbarNav" ] [
             ul [ Class "navbar-nav mr-auto" ] [
-                li [ Class "nav-item dropdown" ][
-                    a [
-                        Id "dropdownMenu1"
-                        Href "#"
-                        DataToggle "dropdown"
-                        AriaHasPopup true
-                        AriaExpanded false
-                        Class "nav-link dropdown-toggle" ]
-                        [ str "Dropdown" ]
-                    ul [    HTMLAttr.Custom ("aria-labelledby", "dropdownMenu1")
-                            Class "dropdown-menu border-0 shadow" ][
-                        dropDownNavItem "Level 1 - Item 1" "#"
-                        dropDownNavItem "Level 1 - Item 2" "#"
-                        li [ Class "dropdown-divider" ] [ ]
-                        dropdownSubMenu "Hover for action" [
-                                    dropDownNavItem "Level 2 - Item 1" "#"
-                                    dropdownSubMenu "Level 2 - Item 2" [
-                                        dropDownNavItem "Level 3 - Item 1" "#"
-                                        dropDownNavItem "Level 3 - Item 2" "#"
-                                    ]
-                                    dropDownNavItem "Level 2 - Item 3" "#"
-                                    dropDownNavItem "Level 2 - Item 4" "#"
-                        ]
-                    ]
-                ]
-
-
-                navItem "About" "#"
-                navItem "Services" "#"
-                navItem "Contact" "#"
+                yield! navTreeFromPaths rootpath files |> generateNavMenus
+                // yield! generateNavMenus fakeMenu
             ]
-
         ]
     ]
             // ul [
