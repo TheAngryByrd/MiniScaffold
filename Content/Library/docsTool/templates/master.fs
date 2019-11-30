137bf5eb973708545fc9436dd2f9a723294f8515
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-11-30 11:35:57 -0500

    Add alt tag to f# logo

--- Content/Library/docsTool/templates/master.fs
+++ Content/Library/docsTool/templates/master.fs
@@ -74,7 +74,7 @@ let renderFooter (cfg : MasterTemplateConfig) =
                             ]
                             li [Class "list-group-item list-group-item-dark ml-0 pl-0"] [
                                 a [Href "http://foundation.fsharp.org/"; Class "text-white"] [
-                                    img [Class "fsharp-footer-logo pr-1"; Src "https://fsharp.org/img/logo/fsharp.svg"]
+                                    img [Class "fsharp-footer-logo pr-1"; Src "https://fsharp.org/img/logo/fsharp.svg"; Alt "FSharp Logo"]
                                     str "F# Software Foundation"
                                 ]
                             ]
