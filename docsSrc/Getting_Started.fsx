0cdabe2e0106586625e232dabf56c5ac5e4b2258
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-12-21 12:27:14 -0500

    Docs tool scaffold for main

--- docsSrc/Getting_Started.fsx
+++
@@ -1,5 +0,0 @@
-(** # code execution *)
-let square x = x * x
-let v = square 34
-(** the value is: *)
-(*** include-value: v ***)
--- docsSrc/index.fsx
+++
@@ -1,5 +0,0 @@
-(** # code execution *)
-let square x = x * x
-let v = square 3
-(** the value is: *)
-(*** include-value: v ***)
