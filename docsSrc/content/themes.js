6307b193b199425435018cadc868e7ae656768f6
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-12-25 18:40:52 -0500

    Support prefers-color-scheme

--- docsSrc/content/themes.js
+++ docsSrc/content/themes.js
@@ -3,13 +3,13 @@ var themes = {
     "light" : {
         "button-text" : "Swap to Dark",
         "button-classes" : "btn btn-dark",
-        "opposite-theme" : "dark",
+        "next-theme" : "dark",
         "body-class" : "bootstrap"
     },
     "dark" : {
         "button-text" : "Swap to Light",
         "button-classes" : "btn btn-light",
-        "opposite-theme" : "light",
+        "next-theme" : "light",
         "body-class" : "bootstrap-dark"
     }
 };
@@ -32,7 +32,7 @@ function setToggleButton(theme) {
     themeToggleButton.textContent = newTheme['button-text'];
     themeToggleButton.className = newTheme['button-classes'];
     themeToggleButton.onclick = function() {
-        setTheme(newTheme['opposite-theme']);
+        setTheme(newTheme['next-theme']);
     }
 }
 
@@ -41,25 +41,39 @@ function setTheme(theme) {
         swapThemeInDom(theme);
     }
     catch(e){
-
     }
     try {
     persistNewTheme(theme);
     }
     catch(e) {
-
     }
     try {
         setToggleButton(theme);
     }
     catch (e) {
-
     }
+}
 
+function getThemeFromStorage() {
+    return window.localStorage.getItem(themeStorageKey);
+}
+
+function getThemeFromScheme() {
+    try {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches){
+            return 'dark';
+        }
+        else {
+            return 'light';
+        }
+    }
+    catch(e) {
+        return null;
+    }
 }
 
 function loadTheme() {
-    var theme = window.localStorage.getItem(themeStorageKey) || 'light';
+    var theme = getThemeFromStorage() || getThemeFromScheme() || 'light';
     setTheme(theme);
 }
 
