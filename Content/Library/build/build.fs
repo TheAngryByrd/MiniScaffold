e68a1399e5927f526864ff41b9a4905773d0dadd
nikoyak <99891966+nikoyak@users.noreply.github.com>
2023-09-05 23:53:45 +0300

    Tuned AssemblyInfo's related things
    - Disabled msbuild's AssemblyInfo.fs generation everywhere
    - AssemblyInfo.fs generation with Fake is left only at the /src level of application and library templates

--- Content/Library/build/build.fs
+++ Content/Library/build/build.fs
@@ -529,7 +529,7 @@ let generateAssemblyInfo _ =
          IO.Path.GetDirectoryName(projectPath),
          (getAssemblyInfoAttributes projectName))
 
-    srcAndTest
+    !!srcGlob
     |> Seq.map getProjectDetails
     |> Seq.iter (fun (projFileName, _, folderName, attributes) ->
         match projFileName with
